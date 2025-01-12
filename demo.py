09ba82e99fed92a3a0df198f1db5b58cade35d1e
Prekshya-raskoti <bisuraskoti@gmail.com>
2025-01-12 18:58:05 +0545

    basics learnt

--- demo.py
+++ demo.py
@@ -1,2 +1,15 @@
 print("Hello World")
 print("Prekshya","Raskoti")
+
+
+print('''25+25''')
+print(25+25)
+
+age = 20
+print(age)
+
+price = 20.09
+print(price)
+
+print("My Age is",age)
+print("The price of pen is:",price)
