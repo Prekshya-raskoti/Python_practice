7cd248b6a432c026ad359dd7b1fe95fd04ff99d4
Prekshya-raskoti <bisuraskoti@gmail.com>
2025-01-12 19:52:59 +0545

    operators type learnt

--- demo.py
+++ demo.py
@@ -13,3 +13,28 @@ print(price)
 
 print("My Age is",age)
 print("The price of pen is:",price)
+
+age = 22
+age2 = age
+print(age2)
+
+
+name = "prekshya"
+name2 = name
+print(name2)
+
+
+print(type(name))
+print(type(age))
+
+old = True or False
+a = None
+
+#print(type(old))
+print(type(a))
+
+
+"""a = 5
+b = 9
+sum = a+b
+print(sum)"""
---
+++ operators.py
@@ -0,0 +1,61 @@
+ # Arithmetic operator
+
+'''a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)'''
+
+# Relational Or Comparsion Operator
+
+'''a = 50
+b = 20
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a<b)
+print(a>=b)
+print(a>b)'''
+
+# Assignment Operrator
+'''num = 10
+#num = num + 10
+num += 10
+print(num)
+
+num1= 20
+num1 -= 20
+print(num1)
+
+num3 = 30
+num3 *= 30
+print(num3)
+
+num4 = 30
+num4 /= 30
+print(num4)
+
+num5 = 30
+num5 %= 20
+print(num5)
+
+num6 = 30
+num6 **= 30
+print(num6)'''
+
+# Logical Operator
+print( not False)
+print( not True)
+
+a = 50
+b = 20
+print(not (a>b))
+
+val1 = True
+val2 = False
+print("Or operator:", val1 or val2)
+print("And operator:", val1 and val2)
+print("Or operator",(a==b) or (a>b))
