a52139f84618c761a4112817eb8a6db447fa4374
Prekshya-raskoti <bisuraskoti@gmail.com>
2025-01-15 20:15:43 +0545

    slice update

--- slicing.py
+++ slicing.py
@@ -1,6 +1,12 @@
 str = "Prekshya Raskoti"
-print(str[1:4])
+'''print(str[1:4])
 print(str[:8])
 print(str[0:8])
 print(str[1:])
-print(str[1:len(str)])
+print(str[1:len(str)])'''
+
+print(str[0:4])
+print(str[: 4])
+
+print(str[5:1])
+print(str[5:len(str)])
