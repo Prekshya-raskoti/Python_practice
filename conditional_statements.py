1aae1a3b77474264e1f1e645b4674d0f2eed9951
Prekshya-raskoti <bisuraskoti@gmail.com>
2025-01-15 21:17:18 +0545

    conditional statements

---
+++ conditional_statements.py
@@ -0,0 +1,23 @@
+age = 25
+if (age >= 18):
+    print("can vote and drive")
+
+
+light = "green"
+if(light == "red"):
+    print("stop")
+
+elif(light == "green"):
+    print("go")
+
+elif(light == "yellow"):
+    print("look")  
+
+
+
+num = 5
+if(num>2):
+    print("number is greater than 2")
+
+if(num>3):
+    print("number is greater than 3")              
