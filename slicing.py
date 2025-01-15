3a26ed095e396b4d7ae1cf5921ed5513b453e0d6
Prekshya-raskoti <bisuraskoti@gmail.com>
2025-01-15 20:39:22 +0545

    string functions learnt

--- slicing.py
+++ slicing.py
@@ -10,3 +10,9 @@ print(str[: 4])
 
 print(str[5:1])
 print(str[5:len(str)])
+
+# Negative slicing
+str1 = "APPLE"
+print(str1[-3:-1])
+
+print(str1[-5:-2])
---
+++ string_functions.py
@@ -0,0 +1,27 @@
+#endswith string
+str = "I am studying python"
+print(str.endswith("app"))
+print(str.endswith("hon"))
+
+#capitalize string
+str1 = "my name is prekshya"
+print(str.capitalize())
+
+str2 = "my name is prekshya"
+str2=str.capitalize()
+print(str2)
+
+#replace string
+str3 ="my name is prekshya"
+print(str3.replace("m","q"))
+print(str3.replace("my","you"))
+
+#find string
+str4 = "I am studying python "
+print(str4.find("o")) # if we search for which do not exist output will be always -1.
+
+#count string
+str5 = "hello world"
+print(str5.count("o"))
+
+
